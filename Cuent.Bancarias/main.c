766590a720552442d9f0a85f0d15f6a61c4fb7ba
Eneko <Eneko@ENEKO-PC>
2017-04-10 01:41:42 +0200

    Dana Bikain. Huts bakarra menuan dago. Teklatutik sartu behar dana lehenago ikusarazten da testua baino.

--- Cuent.Bancarias/main.c
+++ Cuent.Bancarias/main.c
@@ -12,12 +12,18 @@
 int main(void) {
 	Cuentas cu;
 	cu.numCuentas = 6;
-	cu.cuentas[0].cliente = 111;cu.cuentas[0].liquidez = 10000;
-	cu.cuentas[1].cliente = 222;cu.cuentas[1].liquidez = 5000;
-	cu.cuentas[2].cliente = 333;cu.cuentas[2].liquidez = 30000;
-	cu.cuentas[3].cliente = 444;cu.cuentas[3].liquidez = 23012;
-	cu.cuentas[4].cliente = 555;cu.cuentas[4].liquidez = 54623;
-	cu.cuentas[5].cliente = 666;cu.cuentas[5].liquidez = 5642356;
+	cu.cuentas[0].cliente = 111;
+	cu.cuentas[0].liquidez = 10000;
+	cu.cuentas[1].cliente = 222;
+	cu.cuentas[1].liquidez = 5000;
+	cu.cuentas[2].cliente = 333;
+	cu.cuentas[2].liquidez = 30000;
+	cu.cuentas[3].cliente = 444;
+	cu.cuentas[3].liquidez = 23012;
+	cu.cuentas[4].cliente = 555;
+	cu.cuentas[4].liquidez = 54623;
+	cu.cuentas[5].cliente = 666;
+	cu.cuentas[5].liquidez = 5642356;
 
 	//buscarCuenta(&cu, 111);
 	//anadirCuenta(&cu, 5544, 777);
@@ -32,77 +38,105 @@ int main(void) {
 	cuenta1.cliente = 111;
 	cuenta1.liquidez = 75000;
 
-//	int opc = 0, opcu = 0, opct = 0;
-//	do {
-//
-//		printf("MENU DE OPCIONES\n\n");
-//		printf("1. Usuario\n");
-//		printf("2. Trabajador\n");
-//		printf("3. Salir");
-//		fflush(stdout);
-//		printf("Escoge una opcion: \n\n");
-//		scanf("%d", &opc);
-//		fflush(stdout);
-//		switch (opc) {
-//		case 1:
-//			printf("Hola usuario !!!\n\n");
-//			do {
-//				printf("MENU DE CLIENTE");
-//				printf("1. Ingresar dinero en la cuenta.\n");
-//				printf("2. Sacar dinero de la cuenta.\n");
-//				printf("3. Salir");
-//				fflush(stdout);
-//				printf("Escoge una opcion: \n\n");
-//				scanf("%d", &opcu);
-//				switch (opcu) {
-//				case 1:
-//					ingresarDinero(100, cuenta1);
-//					break;
-//				case 2:
-//					sacarDinero(100, cuenta1);
-//					break;
-//				default:
-//					break;
-//				}
-//			} while (opct != 3);
-//			break;
-//		case 2:
-//			printf("Hola trabajador !!!\n\n");
-//			do {
-//				printf("MENU DE TRABAJADOR DEL BANCO");
-//				printf("1. Añadir cuenta\n");
-//				printf("2. Eliminar cuenta.\n");
-//				printf("3. Movimiento de saldo.\n");
-//				printf("4. Buscar Cuenta.\n");
-//				printf("5. Salir");
-//				fflush(stdout);
-//				printf("Escoge una opcion: \n\n");
-//				scanf("%d", &opct);
-//				switch (opct) {
-//				case 1:
-//					anadirCuenta(&cu, 4500, 999);
-//					break;
-//				case 2:
-//					eliminarCuenta1(&cu, 555);
-//					break;
-//				case 3:
-//					transaccion(&cu, 111, 222, 750);
-//					break;
-//				case 4:
-//					buscarCuenta(&cu, 222);
-//					break;
-//				default:
-//					break;
-//				}
-//			} while (opct != 5);
-//			break;
-//		default:
-//			break;
-//		}
-//
-//	} while (opc != 3);
+	int opc = 0, opcu = 0, opct = 0, a, b, c;
+	do {
 
-	eliminarCuenta1(&cu, 333);
+		printf("MENU DE OPCIONES\n\n");
+		printf("1. Usuario\n");
+		printf("2. Trabajador\n");
+		printf("3. Salir");
+		fflush(stdout);
+		printf("Escoge una opcion: \n\n");
+		scanf("%d", &opc);
+		fflush(stdout);
+		switch (opc) {
+		case 1:
+			printf("Hola usuario !!!\n\n");
+			do {
+				printf("MENU DE CLIENTE");
+				printf("1. Ingresar dinero en la cuenta.\n");
+				printf("2. Sacar dinero de la cuenta.\n");
+				printf("3. Salir");
+				fflush(stdout);
+				printf("Escoge una opcion: \n\n");
+				scanf("%d", &opcu);
+				fflush(stdout);
+				switch (opcu) {
+				case 1:
+					fflush(stdout);
+					printf("Introduce el importe: \n");
+					scanf("%d", &a);
+					fflush(stdout);
+					ingresarDinero(a, cuenta1);
+					break;
+				case 2:
+					fflush(stdout);
+					printf("Introduce el importe: \n");
+					scanf("%d", &a);
+					fflush(stdout);
+					sacarDinero(a, cuenta1);
+					break;
+				default:
+					break;
+				}
+			} while (opct != 3);
+			break;
+		case 2:
+			printf("Hola trabajador !!!\n\n");
+			do {
+				printf("MENU DE TRABAJADOR DEL BANCO");
+				printf("1. Añadir cuenta\n");
+				printf("2. Eliminar cuenta.\n");
+				printf("3. Movimiento de saldo.\n");
+				printf("4. Buscar Cuenta.\n");
+				printf("5. Salir");
+				fflush(stdout);
+				printf("Escoge una opcion: \n\n");
+				scanf("%d", &opct);
+				fflush(stdout);
+				switch (opct) {
+				case 1:
+					fflush(stdout);
+					printf("Introduce el cliente: \n");
+					printf("Introduce la liquidez: \n");
+					scanf("%d %d", &a, &b);
+					fflush(stdout);
+					anadirCuenta(&cu, b, a);
+					break;
+				case 2:
+					printf("Introduce el cliente: \n");
+					fflush(stdout);
+					scanf("%d", &a);
+					fflush(stdout);
+					eliminarCuenta1(&cu, a);
+					break;
+				case 3:
+					fflush(stdout);
+					printf("Introduce el cliente A: \n");
+					printf("Introduce el cliente B: \n");
+					printf("Introduce el importe: \n");
+					scanf("%d %d %d", &a, &b, &c);
+					fflush(stdout);
+					transaccion(&cu, a, b, c);
+					break;
+				case 4:
+					fflush(stdout);
+					printf("Introduce el cliente: \n");
+					scanf("%d", &a);
+					fflush(stdout);
+					buscarCuenta(&cu, a);
+					break;
+				default:
+					break;
+				}
+			} while (opct != 5);
+			break;
+		default:
+			break;
+		}
+
+	} while (opc != 3);
+
 	//escribir a fichero
 	FILE *pf;
 	int i;
@@ -117,6 +151,7 @@ int main(void) {
 	}
 
 	fclose(pf);
+	printf("\n\n Gacias por utilizar nuestro producto\n Un Saludo !!!");
 	return 0;
 }
 
